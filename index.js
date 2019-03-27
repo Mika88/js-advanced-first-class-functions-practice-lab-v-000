8e0d6b510e9298971fa4bd298f4ad0f53794ea3e
Mika88 <mikaspivak@gmail.com>
2019-03-27 21:19:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(function(driver) {
+  console.log(driver.name);
+});
+}
+
+function logDriversByHometown(drivers, location) {
+  const callback = function (driver) {
+    if (driver.hometown === location)
+      console.log(driver.name);
+  }
+
+  drivers.forEach(callback);
+}
+
+function driversByRevenue(drivers) {
+  const newDrivers = drivers
+  return newDrivers.sort(function (a, b) {
+    return a.revenue - b.revenue;
+  });
+};
