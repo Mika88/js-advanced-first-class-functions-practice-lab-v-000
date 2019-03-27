3c6df1b1aa35e252bd2c6fcafa5dbc5159acb6e1
Mika88 <mikaspivak@gmail.com>
2019-03-27 21:37:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function driversByRevenue(drivers) {
 };
 
 function driversByName(drivers) {
-drivers.sort(function (a, b) {
+return drivers.slice().sort(function (a, b) {
   return a.localeCompare(b);
  });
 };
